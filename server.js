015bb4539466e4e49b91a6d3ae9928aa4052b703
Márques Niels López Medina <marques@MacBook-Air-de-Marques.local>
2026-03-15 03:24:52 +0100

    Cors de otra manera

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views/pug');
-
-fccTesting(app); // For FCC testing purposes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: '*' }));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views/pug');
 
 app.route('/').get((req, res) => {
   res.render('index');
 });
 
-
+fccTesting(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
