cf2bf9b7a34462c47c07b052fb8deff2943424fe
Márques Niels López Medina <marques@MacBook-Air-de-Marques.local>
2026-03-15 01:40:15 +0100

    Ajuste Lineas Código Pug

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +16,8 @@ app.set('views', __dirname + '/views/pug');
 app.route('/').get((req, res) => {
   res.render('index');
 });
+
+fccTesting(app); //For FCC testing purposes
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
