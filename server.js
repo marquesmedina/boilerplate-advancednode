f06bcdcbc6dac594b2a26b95106c9c6f08a4a08a
Márques Niels López Medina <marques@MacBook-Air-de-Marques.local>
2026-03-15 02:08:11 +0100

    Añado Cors al server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,9 +19,9 @@ app.route('/').get((req, res) => {
   res.render('index');
 });
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Listening on port ' + PORT);
 });
